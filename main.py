f2d0503ef8bb39c4dcf2fa446f77ddd2f1f5b3dc
HenrysElephant1 <jacu9594@colorado.edu>
2019-12-04 14:43:53 -0700

    Added basic main file and path planning file with a couple stub functions.

---
+++ main.py
@@ -0,0 +1,8 @@
+from path_planner import PathPlanner
+
+def main():
+	pp = PathPlanner()
+	pp.plan()
+
+if __name__ == '__main__':
+	main()
---
+++ path_planner.py
@@ -0,0 +1,11 @@
+class PathPlanner():
+	"""docstring for PathPlanner"""
+	def __init__(self):
+		pass
+
+	def plan(self):
+		print("Planning")
+		
+
+if __name__ == '__main__':
+	print("This is a module. Run main.py instead.")
