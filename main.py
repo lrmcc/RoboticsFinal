0ca5ad37be768158eb8c06bbf850af6bc217e975
HenrysElephant1 <jacu9594@colorado.edu>
2019-12-04 15:10:35 -0700

    Added a couple of comments.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+'''
+CSCI 3202 Final Project - Quadcopter Path Planning
+
+Jason Cummings
+Alex Burnley
+Luke McConnell
+Ben Lehfeldt-Ehlinger
+
+Run using python3 main.py
+'''
+
 from path_planner import PathPlanner
 
 def main():
 	pp = PathPlanner()
 	pp.plan()
+
 
 if __name__ == '__main__':
 	main()
--- path_planner.py
+++ path_planner.py
@@ -1,11 +1,15 @@
+'''
+Path Planner
+
+Module that accepts a list of blob values from image processing, then plans and returns a path to be used by the navigation module
+'''
+
 class PathPlanner():
-	"""docstring for PathPlanner"""
 	def __init__(self):
 		pass
 
 	def plan(self):
 		print("Planning")
-		
 
 if __name__ == '__main__':
 	print("This is a module. Run main.py instead.")
