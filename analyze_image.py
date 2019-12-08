0cd001865263367b73a04e2a1b6384928a4b002e
Alex Burnley <albu5828@colorado.edu>
2019-12-08 14:17:41 -0700

    image processing is fully functional, threshold, color masks and detection resolution can be changed as needed

--- analyze_image.py
+++ analyze_image.py
@@ -1,10 +1,14 @@
 
 import cv2
+import math
+import numpy
+
+# colors should be in BGR, not RGB
+colorList = [(133,55,215),(188,214,221),(66,201,133)]
+threshold = 50
 
 
-colorList = [(157,232,255),(210,155,254),(112,239,207),(84,221,177)]
-threshold = 40
-
+blobDetectionResolution = (300,300)
 
 class Blob :
 	averageX = 0
@@ -44,28 +48,12 @@ class Blob :
 
 
 def evaluatePixel(pixel):
-	# print("(" + str(pixel[0]) + "," + str(pixel[1]) + "," + str(pixel[2]))
 	for i in range(0,len(colorList)):
-		diff = abs(colorList[i][0] - pixel[0]) + abs(colorList[i][1] - pixel[1]) + abs(colorList[i][2] - pixel[2])
-		if diff < threshold:
+		if pixel[0] > colorList[i][0] - threshold and pixel[0] < colorList[i][0] + threshold and pixel[1] > colorList[i][1] - threshold and pixel[1] < colorList[i][1] + threshold and pixel[2] > colorList[i][2] - threshold and pixel[2] < colorList[i][2] + threshold:
 			return i+1
-		# if pixel[0] > colorList[i][0] - threshold and pixel[0] < colorList[i][0] + threshold and pixel[1] > colorList[i][1] - threshold and pixel[1] < colorList[i][1] + threshold and pixel[2] > colorList[i][2] - threshold and pixel[2] < colorList[i][2] + threshold:
-		# 	return i+1
 	return 0
 
-# def expand(i,j,pixelList,tmpBlobList,maskValue):
-# 	if pixelList[i][j] == maskValue and pixelList[i][j] != 0:
-# 		print(maskValue)
-# 		tmpBlobList.append((i,j))
-# 		pixelList[i][j] = 0
-# 		if(i > 0):
-# 			expand(i-1,j,pixelList,tmpBlobList,maskValue)
-# 		if(i < len(pixelList)-1):
-# 			expand(i+1,j,pixelList,tmpBlobList,maskValue)
-# 		if(j > 0):
-# 			expand(i,j-1,pixelList,tmpBlobList,maskValue)
-# 		if(j < len(pixelList[i])-1):
-# 			expand(i,j+1,pixelList,tmpBlobList,maskValue)
+
 
 def expand(i,j,pixelList,tmpBlobList,maskValue):
 	if pixelList[i][j] == maskValue and pixelList[i][j] != 0:
@@ -73,13 +61,13 @@ def expand(i,j,pixelList,tmpBlobList,maskValue):
 		pixelList[i][j] = 0
 		queue = []
 		if(i > 0):
-			queue.append((i-1,j)) #expand(i-1,j,pixelList,tmpBlobList,maskValue)
+			queue.append((i-1,j))
 		if(i < len(pixelList)-1):
-			queue.append((i+1,j)) #expand(i+1,j,pixelList,tmpBlobList,maskValue)
+			queue.append((i+1,j)) 
 		if(j > 0):
-			queue.append((i,j-1)) #expand(i,j-1,pixelList,tmpBlobList,maskValue)
+			queue.append((i,j-1))
 		if(j < len(pixelList[i])-1):
-			queue.append((i,j+1)) #expand(i,j+1,pixelList,tmpBlobList,maskValue)
+			queue.append((i,j+1))
 		for p in queue:
 			x = p[0]
 			y = p[1]
@@ -87,14 +75,13 @@ def expand(i,j,pixelList,tmpBlobList,maskValue):
 				tmpBlobList.append((x,y))
 				pixelList[x][y] = 0
 				if(x > 0):
-					queue.append((x-1,y)) #expand(i-1,j,pixelList,tmpBlobList,maskValue)
+					queue.append((x-1,y)) 
 				if(x < len(pixelList)-1):
-					queue.append((x+1,y)) #expand(i+1,j,pixelList,tmpBlobList,maskValue)
+					queue.append((x+1,y))
 				if(y > 0):
-					queue.append((x,y-1)) #expand(i,j-1,pixelList,tmpBlobList,maskValue)
+					queue.append((x,y-1))
 				if(y < len(pixelList[x])-1):
-					queue.append((x,y+1)) #expand(i,j+1,pixelList,tmpBlobList,maskValue)
-
+					queue.append((x,y+1))
 
 
 
@@ -102,17 +89,17 @@ def analyzeImage(image):
 	imageWidth, imageHeight,channels = image.shape
 	# format for list: (x,y)
 	pixelList = []
-	for i in range(imageWidth):
-		pixelList.append([0.0]*imageHeight)
+	for i in range(blobDetectionResolution[0]):
+		pixelList.append([0.0]*blobDetectionResolution[1])
 
-	for i in range(0,imageWidth):
-		for j in range(0, imageHeight):
-			# print(str(image[i,j]))
-			pixelList[i][j] = evaluatePixel(image[i,j])
+	for i in range(0,blobDetectionResolution[0]):
+		row = image[int(float(i) / blobDetectionResolution[0] * imageWidth)]
+		for j in range(0, blobDetectionResolution[1]):
+			pixelList[i][j] = evaluatePixel(row[int(float(j) / blobDetectionResolution[1] * imageHeight)])
 
 	bloblist = []
-	for i in range(0,imageWidth):
-		for j in range(0, imageHeight):
+	for i in range(0,blobDetectionResolution[0]):
+		for j in range(0, blobDetectionResolution[1]):
 			tmpBlobList = []
 			maskValue = pixelList[i][j]
 			expand(i,j,pixelList,tmpBlobList,maskValue)
@@ -120,24 +107,24 @@ def analyzeImage(image):
 				bloblist.append(Blob(tmpBlobList,maskValue))
 	return bloblist
 
-def debug(bloblist, image):
+def debug(bloblist,image):
 	imageWidth, imageHeight,channels = image.shape
-	# format for list: (x,y)
-	pixelList = []
-	for i in range(imageWidth):
-		pixelList.append([0.0]*imageHeight)
-	for i in range(0,imageWidth):
-		for j in range(0, imageHeight):
+	img = numpy.zeros((blobDetectionResolution[0],blobDetectionResolution[1],3),numpy.uint8)#*255
+	for i in range(0,blobDetectionResolution[0]):
+		for j in range(0, blobDetectionResolution[1]):
+			img[i,j] = image[int(float(i) / blobDetectionResolution[0] * imageWidth)][int(float(j) / blobDetectionResolution[1] * imageHeight)]
 			for b in bloblist:
 				if ((i == b.higherBoundingX and j >= b.lowerBoundingY and j <= b.higherBoundingY) or 
 					(i == b.lowerBoundingX and j >= b.lowerBoundingY and j <= b.higherBoundingY) or
 					(j == b.higherBoundingY and i >= b.lowerBoundingX and i <= b.higherBoundingX) or 
 					(j == b.lowerBoundingY and i >= b.lowerBoundingX and i <= b.higherBoundingX)):
-					image[i,j] = (colorList[b.maskValue - 1][0]/2.0,colorList[b.maskValue - 1][1]/2.0,colorList[b.maskValue - 1][2]/2.0)#colorList[b.maskValue - 1]
-	cv2.imwrite('output.png',image)
+					img[i,j] = (255,255,255)#(colorList[b.maskValue - 1][0]*2.0,colorList[b.maskValue - 1][1]*2.0,colorList[b.maskValue - 1][2]*2.0)#colorList[b.maskValue - 1]
+					
+	cv2.imwrite('out.png',img)
 
-testimage = cv2.imread('images-2.png')
-debug(analyzeImage(testimage),testimage)
+
+# testimage = cv2.imread('notes.JPG')
+# debug(analyzeImage(testimage),testimage)
 
 
 
